5025094453b5d887536efc6c7b500db0d7719f6e
gabrantes <gabriel.abrantes@vanderbilt.edu>
2019-08-08 20:01:07 -0500

    Cleaning. Comments.

--- src/components/ChordInput.vue
+++ src/components/ChordInput.vue
@@ -5,7 +5,6 @@
         <VoiceInput
           :id="voice.id"
           :label="voice.label"
-          :category="category"
           :begin-note="voice.beginNote"
           :end-note="voice.endNote"
           :error-state="errorArr[voice.id]"
@@ -35,6 +34,7 @@ export default {
 
   data() {
     return {
+      // Settings for each VoiceInput component
       voiceInputs: [
         {
           id: 0,
@@ -61,6 +61,8 @@ export default {
           endNote: 'C4',
         }
       ],
+
+      // chord voicing data
       voices: [
         {
           id: 0,
@@ -91,96 +93,129 @@ export default {
   },
 
   methods: {
+    /**
+     * Event handler for data emitted by VoiceInputs
+     * @param {Object} emitted The object emitted from the event
+     */
     setVoice: function(emitted) {
-      this.voices[emitted.id].note = emitted.note
-      this.voices[emitted.id].noteInt = emitted.noteInt
-      this.voices[emitted.id].withinRange = emitted.withinRange
+      this.voices[emitted.id].note = emitted.note;
+      this.voices[emitted.id].noteInt = emitted.noteInt;
+      this.voices[emitted.id].withinRange = emitted.withinRange;
     }
   },
 
   computed: {
+    /**
+     * @return {Boolean} true if all 4 VoiceInputs have noteInts
+     */
     allEntered: function () {
       let allEntered = this.voices.reduce(
         (acc, voice) => acc && (voice.noteInt || voice.noteInt === 0), true
-      )
+      );
       return Boolean(allEntered)
     },
 
+    /**
+     * @return {Boolean} true if each noteInt is within its respective range
+     */
     allWithinRange: function() {
-      if (!this.allEntered) return null
+      if (!this.allEntered) return null;
+
       const allWithinRange = this.voices.reduce(
         (acc, voice) => acc && voice.withinRange, true
-      )
+      );
       return allWithinRange;
     },
 
+    /**
+     * For each voice, check if it overlaps with any neighboring voices
+     * @return {Array.<Boolean>} ith element is true if ith voice has voice overlap
+     */
     voiceOverlapArr: function() {
-      if (!this.allEntered) return [null, null, null, null]
+      if (!this.allEntered) return [null, null, null, null];
 
-      let arr = [false, false, false, false]
+      let arr = [false, false, false, false];
       for (let i = 0; i < 4; ++i) {
         if (i < 3) {
           if (this.voices[i].noteInt < this.voices[i+1].noteInt) {
-            arr[i] = true
-            arr[i+1] = true
-          }
-        } else {
-          if (this.voices[i].noteInt > this.voices[i-1].noteInt) {
-            arr[i] = true
-            arr[i-1] = true
+            arr[i] = true;
+            arr[i+1] = true;
           }
         }
       }
 
-      return arr
+      return arr;
     },
 
+    /**
+     * @return {Boolean} true if any voice overlaps are present, false otherwise
+     */
     voiceOverlap: function() {
+      if (!this.allEntered) return null;
+
       const voiceOverlap = this.voiceOverlapArr.reduce(
         (acc, overlap) => acc || overlap, false
-      )
-      return voiceOverlap
+      );
+      return voiceOverlap;
     },    
 
+    /**
+     * For the 3 upper voices, check for any spacing errors
+     * (Spacing error = more than 1 octave between voices)
+     * @return {Array.<Boolean>} ith element is true if ith voice has spacing error
+     */
     spacingErrorArr: function() {
-      if (!this.allEntered) return [null, null, null]
+      if (!this.allEntered) return [null, null, null];
 
-      let arr = [false, false, false]
+      let arr = [false, false, false];
       for (let i = 0; i < 2; ++i) {
         if (this.voices[i].noteInt - this.voices[i+1].noteInt > 12) {
-          arr[i] = true
-          arr[i+1] = true
+          arr[i] = true;
+          arr[i+1] = true;
         }
       }
 
-      return arr
+      return arr;
     },
 
+    /**
+     * @return {Boolean} true if any spacing errors are present, false otherwise
+     */
     spacingError: function() {
+      if (!this.allEntered) return null;
+
       const spacingError = this.spacingErrorArr.reduce(
         (acc, spacing) => acc || spacing, false
       )
-      return spacingError
+      return spacingError;
     },
 
+    /**
+     * Used in setting display state of VoiceInputs
+     * @return {Array.<Boolean>} ith element is true if ith voice has any type of error
+     */
     errorArr: function() {
-      if (!this.allEntered) return [null, null, null, null]
+      if (!this.allEntered) return [null, null, null, null];
 
-      let errorArr = this.voiceOverlapArr
+      let errorArr = this.voiceOverlapArr;
       errorArr = errorArr.map(
         (err, idx) => {
           if (idx < this.spacingErrorArr.length)
-            return (err || this.spacingErrorArr[idx])
-          return err
+            return (err || this.spacingErrorArr[idx]);
+          return err;
         }
       )
-      return errorArr
+      return errorArr;
     },
 
+    /**
+     * Check if chord is valid: (1) all notes in range, (2) no errors
+     * @return {Boolean} true if chord is valid
+     */
     isValid: function() {
-      if (!this.allEntered) return null
+      if (!this.allEntered) return null;
       
-      return (this.allWithinRange && !this.voiceOverlap && !this.spacingError)
+      return (this.allWithinRange && !this.voiceOverlap && !this.spacingError);
     },
   }
 
--- src/components/VoiceInput.vue
+++ src/components/VoiceInput.vue
@@ -23,36 +23,43 @@ export default {
 
     label: String,
 
-    category: {
-      type: String,
-      required: true,
-    },
-
     beginNote: {
+      // first note of valid range, inclusive
       type: String,
       required: true,
     },
 
     endNote: {
+      // last note of valid range, inclusive
       type: String,
       required: true,
     },
 
+    // Note: endNote must be a higher pitch than beginNote
+
     errorState: {
+      // Used in determining display state of text input
       type: Boolean,
     }
   },
 
   data() {
     return {
-      note: '',
+      note: '', // Value in text input -- a pitch represented as a string
       beginInt: this.convertNoteToInt(this.beginNote),
       endInt: this.convertNoteToInt(this.endNote),
     }
   },
 
   methods: {
-    convertNoteToInt: function(val) {
+    /**
+     * Convert a musical pitch from string representation to an integer.
+     * @param {String} note The string representation of a musical pitch, e.g. 'C4'.
+     * @return {Number} The corresponding integer of the pitch.
+     */
+    convertNoteToInt: function(note) {
+      if (!note) return null;
+
       const note_dict = {
         'Cbb': -2,    'Cb': -1,    'C': 0,     'C#': 1,     'C##': 2,
         'Dbb': 0,     'Db': 1,     'D': 2,     'D#': 3,     'D##': 4,
@@ -61,14 +68,13 @@ export default {
         'Gbb': 5,     'Gb': 6,     'G': 7,     'G#': 8,     'G##': 9,
         'Abb': 7,     'Ab': 8,     'A': 9,     'A#': 10,    'A##': 11,
         'Bbb': 9,     'Bb': 10,    'B': 11,    'B#': 12,    'B##': 13
-      };
+      };      
 
-      if (!val) return null;
-
-      const octave = Number(val[val.length - 1]);
-      const note_name = val.slice(0, val.length - 1);
+      const octave = Number(note[note.length - 1]);
+      const note_name = note.slice(0, note.length - 1);
 
       if (isNaN(octave) || !(note_name in note_dict)) {
+        // unable to convert note
         return null;
       } else {
         return note_dict[note_name] + (12 * octave);
@@ -78,36 +84,50 @@ export default {
   },
   
   computed: {
+    /**
+     * Convert value in text input (this.note) to an integer representation
+     * @return {Number} integer representation of this.note
+     */
     noteInt: function() {
       return this.convertNoteToInt(this.note);
     },
 
+    /**
+     * Check if noteInt is within the valid range of the voice
+     * @return {Boolean} true if noteInt is within range, false otherwise
+     */
     withinRange: function() {
       if (this.noteInt || this.noteInt === 0) {
         const withinRange = (this.noteInt >= this.beginInt)
                           && (this.noteInt <= this.endInt);
 
+        // emit voice data
         this.$emit(
-          "set:voice", 
+          'set:voice', 
           {
-            "id": this.id, 
-            "note": this.note, 
-            "noteInt": this.noteInt, 
-            "withinRange": withinRange
+            'id': this.id, 
+            'note': this.note, 
+            'noteInt': this.noteInt, 
+            'withinRange': withinRange
           }
-        )  
+        );  
       
         return withinRange;
       } else {
+        // Invalid input
         return null;
       }
     },
 
+    /**
+     * Determine display state of text input, shows errors if any
+     * @return {Boolean}
+     */
     inputState: function() {
       if (this.noteInt && this.errorState !== null)
-        return !this.errorState
+        return !this.errorState;
       else
-        return null
+        return null;
     }
   }
 

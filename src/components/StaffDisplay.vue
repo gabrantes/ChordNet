ee205e512e4f07a7f91375d555a45197c71f6a1b
gabrantes <gabriel.abrantes@vanderbilt.edu>
2019-08-10 13:49:33 -0500

    drawChord

--- src/components/StaffDisplay.vue
+++ src/components/StaffDisplay.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div id="display"/>
-    <b-button @click="drawNotes">Add Notes</b-button>
+    <b-button @click="drawChord">Add Notes</b-button>
     <b-button variant="danger" @click="drawEmptySystem">Clear System</b-button>
   </div>  
 </template>
@@ -16,11 +16,40 @@ export default {
     return {
       vf: null,
       context: null,
-      notes: [
-        {clef: "treble", keys: ["c/4"], duration: "q"},
-        {clef: "treble", keys: ["e/4"], duration: "q"},
-        {clef: "treble", keys: ["g/4"], duration: "q"},
+      chord: [
+        {
+          id: 0,
+          note: 'A4',
+        },
+        {
+          id: 1,
+          note: 'C#4',
+        },
+        {
+          id: 2,
+          note: 'E3',
+        },
+        {
+          id: 3,
+          note: 'A2',
+        },
       ],
+      vfNotes: [
+        {clef: "treble", keys: ["A/4"], duration: "q", stem_direction: 1},
+        {clef: "treble", keys: ["C#/4"], duration: "q", stem_direction: -1},
+        {clef: "bass", keys: ["E/3"], duration: "q", stem_direction: 1},
+        {clef: "bass", keys: ["A/2"], duration: "q", stem_direction: -1},
+      ],
+      mNotes: {
+        treble: [
+          {clef: "treble", keys: ["A/4"], duration: "q"},
+          {clef: "treble", keys: ["C#/4"], duration: "q"},
+        ],
+        bass: [
+          {clef: "bass", keys: ["E/3"], duration: "q"},
+          {clef: "bass", keys: ["A/2"], duration: "q"},
+        ]
+      },
       trebleStave: null,
       bassStave: null,
     }
@@ -36,14 +65,14 @@ export default {
   methods: {
     createTrebleStave: function() {
       let VF = this.vf;
-      let trebleStave = new VF.Stave(10, 40, 200);
+      let trebleStave = new VF.Stave(10, 40, 100); // (x, y, width)
       trebleStave.addClef("treble");
       return trebleStave;
     },
 
     createBassStave: function() {
       let VF = this.vf;
-      let bassStave = new VF.Stave(10, 120, 200);
+      let bassStave = new VF.Stave(10, 150, 100); // (x, y, width)
       bassStave.addClef("bass");
       return bassStave;
     },
@@ -52,7 +81,7 @@ export default {
       let VF = this.vf;
       let div = document.getElementById("display");
       let renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-      renderer.resize(250, 300);
+      renderer.resize(150, 300);  // (width, height)
       let context = renderer.getContext();
       context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
       return context;
@@ -73,17 +102,36 @@ export default {
       return;
     },
 
-    drawNotes: function() {
+    drawChord: function() {
       let VF = this.vf;
-      this.drawEmptySystem();
-      let notes = this.notes.map((note) => new VF.StaveNote(note));
-      let voice = new VF.Voice({num_beats: 3, beat_value: 4});
-      voice.addTickables(notes);
-      // eslint-disable-next-line
-      let formatter = new VF.Formatter().joinVoices([voice]).format([voice], 200);
+      this.drawEmptySystem();   
+      
+      let voice_settings = {num_beats: 1, beat_value: 4};
+      let voices = [];
+      for (let i = 0; i < 4; ++i) {
+        voices.push(new VF.Voice(voice_settings));
+      }
+      
+      let notes = this.vfNotes.map((note) => new VF.StaveNote(note));
+      voices = voices.map(
+        (voice, index) => {
+          let stave = (index < 2) ? this.trebleStave : this.bassStave;
+          voice.addTickables([notes[index]]).setStave(stave);
+          return voice;
+        }
+      )
+
+      let formatter = new VF.Formatter();
+      for (let i = 0; i < 4; ++i) {
+        formatter.joinVoices([voices[i]]);
+      }
+      formatter.format(voices, 50);
+
       // draw on current renderer/context
-      voice.draw(this.context, this.trebleStave);
-    }
+      for (let i = 0; i < 4; ++i) {
+        voices[i].setContext(this.context).draw();
+      }
+    },
   }
 }
 </script>

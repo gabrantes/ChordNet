a1a38ccef9dbcab4bbe665d99ce11a7c8d47c323
gabrantes <gabriel.abrantes@vanderbilt.edu>
2019-08-12 21:40:52 -0500

    Setup backend connection

--- src/App.vue
+++ src/App.vue
@@ -1,116 +1,30 @@
 <template>
   <div id="app">
     <MyNavBar class="shadow"/>
-    <ProgressionDisplay
-      id="display"
-      :inputCurChord="curChordDisplay"
-      :keySignature="keyStr"/>
-    <MasterInput 
-      :disabled="disableInputs"
-      @send:cur-chord-display="toCurDisplay"
-      @send:key-display="toKeyDisplay"
-      @request:backend="requestBackend"
-      id="inputs"/>
+    <HomePage />
   </div>
 </template>
 
 <script>
-// import axios from 'axios'
 import MyNavBar from './components/MyNavBar.vue'
-import ProgressionDisplay from './components/ProgressionDisplay.vue'
-import MasterInput from './components/MasterInput.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'app',
 
   components: {
     MyNavBar,
-    ProgressionDisplay,
-    MasterInput,
+    HomePage,
   },
-
-  data() {
-    return {
-      curChordDisplay: null,
-      keyStr: 'C',
-      disableInputs: false,
-    }
-  },
-
-  methods: {
-    toCurDisplay: function(curChordDisplay) {
-      this.curChordDisplay = curChordDisplay;
-    },
-
-    toKeyDisplay: function(keyStr) {
-      this.keyStr = keyStr;
-    },
-
-    requestBackend: function(modelInputs) {
-      this.disableInputs = true;
-      console.log(modelInputs);
-
-      // simulate backend request
-      setTimeout(() => {this.disableInputs = false;}, 2000);
-
-      /* AXIOS
-      const url = 'https://ml.googleapis.com/v1/projects/chordnet-backend/models/chordnet_verbose/versions/slim_custom_v6:predict';
-      const reqBody = {
-        'instances': [
-          modelInputs
-        ]
-      };
-      
-      axios.post(url, reqBody)
-        .then(response => {
-          console.log(response);
-          this.disableInputs = false;
-          })
-        .catch(e => {
-          console.log(e);
-          this.disableInputs = false;
-          });
-      */
-
-      return;
-    }
-  }
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
 #app {
-  width: 100%;
-  min-width: 360px;
-  height: 100vh;
-  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
-  font-family: 'Roboto', sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;  
-  padding-bottom: 20px;
-}
-
-#display {
-  position: -webkit-sticky; /* Safari */
-  position: sticky;
-  top: 0;
-  z-index: 2;
-  max-width: 360px;
-  margin: auto;
-  margin-top: 10px;
-  border-radius: 6px;
-}
-
-#inputs {
-  margin: auto;
-  height: calc(50vh + 25%);
-  overflow: auto;
-  z-index: 1;
-  padding-bottom: 20px;
-  /* border: 5px solid #2c3e50;
-  border-radius: 6px 6px 0px 0px; */
+  color: #2c3e50;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,113 @@
+<template>
+  <div id="app">
+    <ProgressionDisplay
+      id="display"
+      :inputCurChord="curChordDisplay"
+      :inputNextChord="nextChordDispay"
+      :keySignature="keyStr"/>
+    <MasterInput 
+      :disabled="disableInputs"
+      @send:cur-chord-display="toCurDisplay"
+      @send:key-display="toKeyDisplay"
+      @request:backend="requestBackend"
+      id="inputs"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import ProgressionDisplay from './ProgressionDisplay.vue'
+import MasterInput from './MasterInput.vue'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    ProgressionDisplay,
+    MasterInput,
+  },
+
+  data() {
+    return {
+      curChordDisplay: null,
+      nextChordDisplay: null,
+      keyStr: 'C',
+      disableInputs: false,
+    }
+  },
+
+  methods: {
+    toCurDisplay: function(curChordDisplay) {
+      this.curChordDisplay = curChordDisplay;
+    },
+
+    toKeyDisplay: function(keyStr) {
+      this.keyStr = keyStr;
+    },
+
+    requestBackend: function(modelInputs) {
+      this.disableInputs = true;
+      console.log(modelInputs);
+
+      // simulate backend request
+      // setTimeout(() => {this.disableInputs = false;}, 2000);
+
+      const url = 'api/predict';
+      const reqBody = {
+        'instances': [
+          modelInputs
+        ]
+      };
+      
+      axios.post(url, reqBody)
+        .then(response => {
+          console.log(response.data);
+          this.nextChordDisplay = response.data;
+          this.disableInputs = false;
+          })
+        .catch(e => {
+          console.log(e);
+          this.disableInputs = false;
+          });
+
+      return;
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+#app {
+  width: 100%;
+  min-width: 360px;
+  height: 100vh;
+  font-family: 'Roboto', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;  
+  padding-bottom: 20px;
+}
+
+#display {
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+  z-index: 2;
+  max-width: 360px;
+  margin: auto;
+  margin-top: 10px;
+  border-radius: 6px;
+}
+
+#inputs {
+  margin: auto;
+  height: calc(50vh + 25%);
+  overflow: auto;
+  z-index: 1;
+  padding-bottom: 20px;
+  /* border: 5px solid #2c3e50;
+  border-radius: 6px 6px 0px 0px; */
+}
+</style>
